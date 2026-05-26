73eb974bb35afc7beb3875c1b7a971c70091714a
levon-brokenponyclub <levon@brokenpony.club>
2026-05-26 09:51:09 +0200

    Spelling

--- components/sections/ProductSection.tsx
+++ components/sections/ProductSection.tsx
@@ -136,7 +136,7 @@ export default function ProductSection() {
               }
             />
             <span className="w-2 h-2 bg-[#1D96FC] rounded-full"></span>
-            Product thats work
+            Products that work
           </div>
           <h2 className="font-heading text-3xl md:text-4xl font-medium text-white text-center lg:text-left tracking-tight mb-6">
             Building{" "}
